7d177de1d671c77584c80760c2597e93c8db135f
Katherine Pearson <katherine@Katherines-MacBook-Pro-2.local>
2021-04-14 13:54:30 -0500

    katherine

--- Client/resources/scripts/cartfunc.js
+++ Client/resources/scripts/cartfunc.js
@@ -7,9 +7,9 @@ function getCart(){
     }).then(function(json){
         let html = "<ul>";
         json.forEach((CartItem)=>{
-            html += "<li>" +"<button onclick=\"addCart("+cart.cartid+")\">+</button>",
-            html += cart.itemName,
-            html += "<button onclick=\"delCart("+cart.cartid+")\">-</button>" + "</li>"
+            html += "<li>" +"<button onclick=\"addCart("+CartItem.cartid+")\">+</button>",
+            html += CartItem.itemName,
+            html += "<button onclick=\"delCart("+CartItem.cartid+")\">-</button>" + "</li>"
         })
         html += "</ul>";
         document.getElementById("cartitems").innerHTML = html;
@@ -49,7 +49,14 @@ function delCart(cartid){
 }
 function chickenparmfunction(){
     const postCartAPIURL = "https://localhost:5000/api/cartAPI";
-    const itemPost = "chickenparm";
+    var itemPost = "chicken parmesan";
+    itemPost = itemPost.toString();
+    var item = {
+        cartid: 2,
+        itemName: "chicken parmesan",
+        price: 1,
+        quantity: 2
+    }
     console.log(itemPost);
     fetch(postCartAPIURL, {
         method: "POST",
@@ -57,9 +64,122 @@ function chickenparmfunction(){
             "Accept": 'application/json',
             "Content-Type": 'application/json',
         },
-        body: JSON.stringify({
-          itemName: itemPost
-        })
+        body: JSON.stringify(item)
+    }).then((response)=>{
+        console.log(response);
+        getCart();
+    })
+}
+function chocchipfunction(){
+    const postCartAPIURL = "https://localhost:5000/api/cartAPI";
+    var itemPost = "chocolate chip cookie";
+    itemPost = itemPost.toString();
+    var item = {
+        cartid: 2,
+        itemName: "chocolate chip cookie(s)",
+        price: 1,
+        quantity: 2
+    }
+    console.log(itemPost);
+    fetch(postCartAPIURL, {
+        method: "POST",
+        headers: {
+            "Accept": 'application/json',
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(item)
+    }).then((response)=>{
+        console.log(response);
+        getCart();
+    })
+}
+function vegburgfunction(){
+    const postCartAPIURL = "https://localhost:5000/api/cartAPI";
+    var itemPost = "veggie burger";
+    itemPost = itemPost.toString();
+    var item = {
+        cartid: 2,
+        itemName: "Veggie Burger",
+        price: 1,
+        quantity: 2
+    }
+    console.log(itemPost);
+    fetch(postCartAPIURL, {
+        method: "POST",
+        headers: {
+            "Accept": 'application/json',
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(item)
+    }).then((response)=>{
+        console.log(response);
+        getCart();
+    })
+}
+function chicksaladfunction(){
+    const postCartAPIURL = "https://localhost:5000/api/cartAPI";
+    var itemPost = "chicken salad sandwich";
+    itemPost = itemPost.toString();
+    var item = {
+        cartid: 2,
+        itemName: "chicken salad sandwich",
+        price: 1,
+        quantity: 2
+    }
+    console.log(itemPost);
+    fetch(postCartAPIURL, {
+        method: "POST",
+        headers: {
+            "Accept": 'application/json',
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(item)
+    }).then((response)=>{
+        console.log(response);
+        getCart();
+    })
+}
+function turkfunction(){
+    const postCartAPIURL = "https://localhost:5000/api/cartAPI";
+    var itemPost = "turkey melt";
+    itemPost = itemPost.toString();
+    var item = {
+        cartid: 2,
+        itemName: "Turkey Melt",
+        price: 1,
+        quantity: 2
+    }
+    console.log(itemPost);
+    fetch(postCartAPIURL, {
+        method: "POST",
+        headers: {
+            "Accept": 'application/json',
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(item)
+    }).then((response)=>{
+        console.log(response);
+        getCart();
+    })
+}
+function housesaladfunction(){
+    const postCartAPIURL = "https://localhost:5000/api/cartAPI";
+    var itemPost = "house salad";
+    itemPost = itemPost.toString();
+    var item = {
+        cartid: 2,
+        itemName: "house salad",
+        price: 1,
+        quantity: 2
+    }
+    console.log(itemPost);
+    fetch(postCartAPIURL, {
+        method: "POST",
+        headers: {
+            "Accept": 'application/json',
+            "Content-Type": 'application/json',
+        },
+        body: JSON.stringify(item)
     }).then((response)=>{
         console.log(response);
         getCart();
