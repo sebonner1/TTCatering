5dd9a6b930b4b0c12a5ad21c7da9a6081628ad90
Sydney Bonner <sebonner1@crimson.ua.edu>
2021-04-14 10:28:19 -0500

    calendar

--- Client/resources/scripts/receipt.js
+++ Client/resources/scripts/receipt.js
@@ -1,13 +1,15 @@
 const checkoutbutton = document.getElementById("checkoutbutton");
 const submitbutton = document.getElementById("submitbutton");
+console.log(submitbutton);
 
 // the below syntax from research
 checkoutbutton.addEventListener("click", (e) => {
     alert("You are being directed to the calendar.");
     window.location.href= "../resources/Calendar.html";
 })
+
 // the below syntax from research
-submitbutton.addEventListener("click", (e) => {
+submitbutton.addEventListener("click", (a) => {
     alert("You are being directed to the receipt.");
     window.location.href= "../resources/Receipt.html";
 })
