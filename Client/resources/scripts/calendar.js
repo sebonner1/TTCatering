f463870cf99d856725982fb061127e0c07b5d0d0
Sydney Bonner <sebonner1@crimson.ua.edu>
2021-04-14 17:22:13 -0500

    values

--- Client/resources/scripts/calendar.js
+++ Client/resources/scripts/calendar.js
@@ -2,10 +2,32 @@
     var day = document.getElementById("weekdays_short");
     var timeSel = document.getElementById("time");
     var addressSel = document.getElementById("address");
+    loadTimes();
   
     // The manager login is below, working on taking it to the next page, the dashboard
     if (day === "S") {
         alert("You have successfully logged in.");
         window.location.href= "../resources/ManagerDashboard";
     } 
+}
+
+function loadTimes()
+{
+  var timeSel = document.getElementById("time");
+  timeSel.innerHTML = `
+  <option value="" selected="selected">Please select a day first</option>
+  <option value="7:00 AM">7:00 AM</option>
+  <option value="8:00 AM">8:00 AM</option>
+  <option value="9:00 AM">9:00 AM</option>
+  <option value="10:00 AM">10:00 AM</option>
+  <option value="11:00 AM">11:00 AM</option>
+  <option value="12:00 PM">12:00 PM</option>
+  <option value="1:00 PM">1:00 PM</option>
+  <option value="2:00 PM">2:00 PM</option>
+  <option value="3:00 PM">3:00 PM</option>
+  <option value="4:00 PM">4:00 PM</option>
+  <option value="5:00 PM">5:00 PM</option>
+  <option value="6:00 PM">6:00 PM</option>
+  <option value="7:00 AM">7:00 PM</option>
+  `;
 }
--- Client/resources/scripts/index.js
+++ Client/resources/scripts/index.js
@@ -23,4 +23,12 @@ function ClearAll() {
     // clear the cart here
     doShowAll();
 }
+/*
+const checkoutbutton = document.getElementById("checkoutbutton");
 
+// the below syntax from research
+checkoutbutton.addEventListener("click", (e) => {
+    alert("You are being directed to the calendar.");
+    window.location.href= "../resources/Calendar.html";
+})
+*/
